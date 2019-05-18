ac7450fd309242781e23b69faf83879b2678d182
Noah Fairbairn <noahfairbairn@gmail.com>
2019-05-18 13:19:50 -0700

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,33 @@
-class Artist
+require 'pry'
 
-    attr_reader :name
+
+class Artist
+    attr_accessor :name, :artist
+    @@songs = []
 
     def initialize(name)
-     @name = name
+      @name = name
+      
+    end
+   
+    def add_song(song)
+      @@songs << song
+      song.artist = self
+    end
+   
+    def songs
+      @@songs
+    end
+
+    def add_song_by_name(name)
+        song = Song.new(name)
+        @@songs << song
+        song.artist = self
+    end
+
+    def self.song_count
+        @@songs.length
     end
 
 
-end
+  end
--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,32 @@
+require 'pry'
+
 class Author
 
+    attr_accessor :name, :author, :title
     
+    @@posts = []
 
+    def initialize(name)
+        @name = name
+    end
+
+    def posts
+        @@posts
+    end
+
+    def add_post(post)
+        post.author = self
+        @@posts << post
+    end
+
+    def add_post_by_title(title)
+        post = Post.new(title)
+        @@posts << post
+        post.author = self
+    end
+
+    def self.post_count
+        @@posts.length
+    end
 
 end
